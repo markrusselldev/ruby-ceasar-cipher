59eb4095a56caab69d07754ec88b549facfa03b8
markrusselldev <mark@markrussell.dev>
2023-01-05 21:01:29 -0700

    feat: remove ! from .betwen check
    reduce checks needed for normal stepping in ternary

--- ceasar-cipher.rb
+++ ceasar-cipher.rb
@@ -3,14 +3,14 @@ def ceasar_cipher(string, step)
   # can use .unpack() or .bytes to turn our letters into ASCII
   string.unpack("C*").map do |char| 
     # pass in spaces and special chars without stepping
-    if char.between?(32, 64) || 
-        char.between?(91, 96) || 
-        char > 122 then
+    if char.between?(32, 64) || char.between?(91, 96) || char > 122 then
       charArray.push(char)
     else
-    charArray.push((char + step < 123 && !(char + step).between?(32, 64) && !(char + step).between?(91, 96))?(char + step):(char + step - 26))
+    # step normally if stepping lands on a letter, wrap if not
+    charArray.push(((char + step).between?(65, 90) || (char + step).between?(97, 122))?(char + step):(char + step - 26))
     end
   end
+  # turn ASCII back into a string of letters
   charArray.pack("C*")
 end
 puts ceasar_cipher("What a string!", 5)
